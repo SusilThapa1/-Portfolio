60ceffe7e334f6a6c32acf460b563f966beb6081
SusilThapa1 <shrishthapaa@gmail.com>
2025-06-26 18:14:40 +0545

    Changes projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,40 +4,35 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'project1': "url('images/project-1.jpg')",
-        'project2': "url('images/project-2.jpg')",
-        'project3': "url('images/project-3.jpg')",
-        'project4': "url('images/project-4.jpg')",
-        'project5': "url('images/project-5.jpg')",
-        'project6': "url('images/project-6.jpg')",
-        'available':"url('images/available.jpg')",
-         
+        project1: "url('images/proj1.png')",
+        project2: "url('images/proj2.png')",
+        project3: "url('images/proj3.png')",
+        project4: "url('images/proj4.png')",
+        project5: "url('images/proj5.png')",
+        project6: "url('images/proj6.png')",
+        available: "url('images/available.jpg')",
       },
-      colors:{
-        color1:'#FFBD39',
-        color2:'#999999',
-        color3:'#FFFFFF1A',
+      colors: {
+        color1: "#FFBD39",
+        color2: "#999999",
+        color3: "#FFFFFF1A",
       },
-      fontFamily:{
-        poppins:['Poppins']
+      fontFamily: {
+        poppins: ["Poppins"],
       },
-      keyframes:{
-        progress:{
-          '0%':{width: '0'},
-          '100%':{width:'100%'}
+      keyframes: {
+        progress: {
+          "0%": { width: "0" },
+          "100%": { width: "100%" },
         },
-        rotate:{
-          'to':{transform:'rotate'
-          }
-        }
+        rotate: {
+          to: { transform: "rotate" },
+        },
       },
-      animation:{
-        'progress':'progress 3s  ease-in-out infinite ' 
-
-      }
+      animation: {
+        progress: "progress 3s  ease-in-out infinite ",
+      },
     },
   },
-  plugins:  [],
-}
-
-
+  plugins: [],
+};
